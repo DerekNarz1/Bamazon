22d4b743001e96e2cfff3eabdfe52f1e437f570d
DerekNarz1 <dereknarz1@gmail.com>
2017-08-28 13:23:18 -0700

    added Final Sql

--- bamazon.sql
+++ bamazon.sql
@@ -14,4 +14,5 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-values(
+values('Ultra Boosts', 'Shoes', 180.00, 100),('Stan Smiths', 'Shoes', 90.00, 100),('Advil','Medicine', 5.75 , 1000), ('Toilet Paper', 'Grocery', 14.99, 400), ('Apples', 'Grocery', 0.50, 1000),
+('Air Force 1', 'Shoes', 90.00, 1000), ('i7 Processor', 'Tech', 300.00, 50),('1080Ti', 'Tech', 450.00, 75),('K70 RGB', 'Tech', 250.99, 50),('M65 RGB', 'Tech', 150.99, 75);
